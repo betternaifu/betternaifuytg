5ac33986f94f088029a617571d18ee150cb83b97
betternaifu <129021248+betternaifu@users.noreply.github.com>
2025-01-26 04:12:47 -0500

    Comment out unused import

--- src/utils/chrome/index.js
+++ src/utils/chrome/index.js
@@ -1,6 +1,6 @@
 import _LocalStorage from './LocalStorage'
 import _SyncStorage from './SyncStorage'
-import _Notifications from './Notifications'
+// import _Notifications from './Notifications'
 
 // export default {
 //   LocalStorage: new _LocalStorage,
@@ -10,4 +10,4 @@ import _Notifications from './Notifications'
 
 export const LocalStorage = new _LocalStorage()
 export const SyncStorage = new _SyncStorage()
-export const Notifications = new _Notifications()
+// export const Notifications = new _Notifications()
